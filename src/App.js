9e4b8d8a162b0c813147826ce4c90dcc9acb8ee5
uf0 <scnepa222@gmail.com>
2020-06-15 18:21:32 +0200

    added languages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import i18n from 'i18next'
-import { initReactI18next, useTranslation } from 'react-i18next'
-import { Miller } from './miller'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Outline from './pages/Outline'
+import React from "react";
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import { Miller } from "./miller";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Outline from "./pages/Outline";
 
-const LANGS = ['fr_FR', 'de_DE', 'en_EN']
+const LANGS = ["fr_FR", "de_DE", "en_US", "nl_BE"];
 
 const resources = {
   fr_FR: {
     translation: {
-      hello: 'Bonjour',
+      hello: "Bonjour",
     },
   },
-  en_EN: {
+  en_US: {
     translation: {
-      hello: 'Hello',
+      hello: "Hello",
     },
   },
   de_DE: {
     translation: {
-      hello: 'Hallo',
+      hello: "Hallo",
     },
   },
-}
+  nl_BE: {
+    translation: {
+      hello: "Hallo",
+    },
+  },
+};
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en_EN',
+    lng: "en_US",
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
-  })
+  });
 
 function AppRoutes() {
   return (
@@ -53,16 +58,16 @@ function AppRoutes() {
         <Outline />
       </Route>
     </Switch>
-  )
+  );
 }
 
 export default function App() {
-  const { i18n } = useTranslation()
+  const { i18n } = useTranslation();
   return (
-    <Miller lang={i18n.language} langs={LANGS} apiUrl={'/api'} cache suspense>
+    <Miller lang={i18n.language} langs={LANGS} apiUrl={"/api"} cache suspense>
       <Router>
         <AppRoutes />
       </Router>
     </Miller>
-  )
+  );
 }
