c9031540d42cbd7d15731227752fb4b5112e0dd1
gffuma <fumagalli.gf@gmail.com>
2020-06-30 21:06:38 +0200

    Working on Otline doc playing

--- src/pages/Outline/Outline.js
+++ src/pages/Outline/Outline.js
@@ -31,6 +31,11 @@ const SeekLine = ({ index, progress, onSeek, title, subtitle }) => {
   )
 }
 
+function convertStrToSeconds(str) {
+  const [mins, secs] = str.split(':')
+  return parseInt(mins) * 60 + parseInt(secs)
+}
+
 export default function Outline() {
   const [outlineStory] = useCacheStory('outline')
   const [outlineTheme] = useCacheStory('outline-1', {
@@ -41,22 +46,55 @@ export default function Outline() {
   const [chapterIndex, setChapterIndex] = useState(0)
   const [playing, setPlaying] = useState(false)
   const togglePlay = () => setPlaying((a) => !a)
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState({
+    played: 0,
+    playedSeconds: 0,
+  })
   const playerRef = useRef()
 
   const playingVideoUrl = useMemo(() => {
     const chapter = chapters[chapterIndex]
     const documentId = chapter.contents.modules[0].object.id
     const docVideo = find(chapter.documents, { document_id: documentId })
-    const videoUrl = docVideo.data.translated_urls
-    return videoUrl
+    return docVideo.data.translated_urls
   }, [chapterIndex, chapters])
 
-  function handleSeek(index, progress) {
+  function handleSeek(index, progressFraction) {
     setChapterIndex(index)
-    setProgress(progress)
-    playerRef.current.seekTo(progress, 'fraction')
+    setProgress({
+      played: progressFraction,
+      playedSeconds: null, // Will auto set by my player
+    })
+    playerRef.current.seekTo(progressFraction, 'fraction')
   }
+
+  const seekObjectsSeconds = useMemo(() => {
+    const chapter = chapters[chapterIndex]
+    const seekObjects = chapter.contents.modules[0].objects
+    return seekObjects.map((o) => ({
+      ...o,
+      from: convertStrToSeconds(o.from),
+      to: convertStrToSeconds(o.to),
+    }))
+  }, [chapterIndex, chapters])
+
+  const playedSeconds = progress.playedSeconds
+
+  const playingDocuementId = useMemo(() => {
+    if (playedSeconds === null) {
+      return null
+    }
+    const objInTime = find(
+      seekObjectsSeconds,
+      (o) => playedSeconds >= o.from && o.to <= playedSeconds
+    )
+    if (objInTime) {
+      return objInTime.id
+    }
+    return null
+  }, [seekObjectsSeconds, playedSeconds])
+
+  console.log('SS', playedSeconds, seekObjectsSeconds, playingDocuementId)
 
   return (
     <div className={styles.PlayerPage}>
@@ -65,12 +103,15 @@ export default function Outline() {
         <ReactPlayer
           className={styles.Player}
           ref={playerRef}
-          onProgress={(p) => setProgress(p.played)}
+          onProgress={setProgress}
           onEnded={() => {
             if (chapterIndex + 1 < chapters.length) {
               setChapterIndex(chapterIndex + 1)
               playerRef.current.seekTo(0, 'fraction')
-              setProgress(0)
+              setProgress({
+                played: 0,
+                playedSeconds: null,
+              })
             }
           }}
           width="100%"
@@ -98,7 +139,7 @@ export default function Outline() {
                 index={i}
                 title={chapter.data.title}
                 subtitle={'1970-1977'}
-                progress={i === chapterIndex ? progress : null}
+                progress={i === chapterIndex ? progress.played : null}
               />
             </div>
           ))}
