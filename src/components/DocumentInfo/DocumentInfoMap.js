01bd2363a2b670542702e173d648f01643dc6fa7
gffuma <fumagalli.gf@gmail.com>
2020-07-01 17:57:12 +0200

    DocumentInfo image

--- src/components/DocumentInfo/DocumentInfoMap.js
+++ src/components/DocumentInfo/DocumentInfoMap.js
@@ -5,8 +5,8 @@ export default function DocumentInfoMap({ doc }) {
   return (
     <div className="row no-gutters h-100 py-4">
       <div className="col-md-5 offset-md-2">
-        <div className={styles.DocumentInfoMap}>
-          <div className={styles.DocumentInfoMapImageContainer}>
+        <div className={styles.MapImageContainer}>
+          <div className={styles.MapImageWrapper}>
             <img
               title={doc.title}
               alt={doc.title}
@@ -16,9 +16,9 @@ export default function DocumentInfoMap({ doc }) {
         </div>
       </div>
       <div className='col-md-3'>
-        <div className={styles.DocumentInfoMapTextContainer}>
-          <div className={styles.DocumentInfoMapTextBox}>
-            <div className={styles.DocumentInfoMapLabel}>Map</div>
+        <div className={styles.InfoBoxContainer}>
+          <div>
+            <div className={styles.InfoLabel}>Map</div>
             <h2>{doc.data.title}</h2>
             <p>{doc.data.description}</p>
           </div>
