fc6a6f42d90b059c3309321494192b409712688f
gffuma <fumagalli.gf@gmail.com>
2020-07-02 15:58:44 +0200

    Vanilla video in DocumentInfoVideo

--- src/components/DocumentInfo/DocumentInfoVideo/DocumentInfoVideo.js
+++ src/components/DocumentInfo/DocumentInfoVideo/DocumentInfoVideo.js
@@ -1,26 +1,14 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
 import styles from './DocumentInfoVideo.module.scss'
 import DocumentInfoBox from '../DocumentInfoBox'
 
 export default function DocumentInfoVideo({ doc }) {
-  const WrapPlayer = React.forwardRef(({ children, ...props }, ref) => (
-    <div className={styles.PlayerWrapper} {...props} ref={ref}>
-      {children}
-      <div className={styles.Controls}>MayBe Control Ma Video!</div>
-    </div>
-  ))
-
   return (
     <div className={styles.VideoContainer}>
       <div className={styles.PlayerContainer}>
-        <ReactPlayer
-          wrapper={WrapPlayer}
-          width={null}
-          height={null}
-          url={doc.url}
-          controls
-        />
+        <div className={styles.PlayerWrapper}>
+          <video src={doc.url} controls disablePictureInPicture />
+        </div>
       </div>
       <DocumentInfoBox doc={doc} />
     </div>
