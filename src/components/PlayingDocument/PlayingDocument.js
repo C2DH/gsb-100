3ccd8d54044338fa6255b89e4e0804119354d1f3
uf0 <scnepa222@gmail.com>
2020-07-02 17:36:45 +0200

    style outline

---
+++ src/components/PlayingDocument/PlayingDocument.js
@@ -0,0 +1,20 @@
+import React, { useState, useCallback } from 'react'
+import OutlineDocumentModal from '../OutlineDocumentModal'
+import styles from './PlayingDocument.module.scss'
+
+function PlayingDocument({ document }) {
+  const [showModal, setShowModal] = useState(false)
+  const toggleModal = useCallback(() => setShowModal((a) => !a), [])
+  return (
+    <>
+      <div className={`${styles.PlayingDocument} m-5`} onClick={toggleModal}>
+        <img src={document.data.translated_thumb_urls} alt={document.title} />
+      </div>
+      {showModal && (
+        <OutlineDocumentModal doc={document} onClose={toggleModal} />
+      )}
+    </>
+  )
+}
+
+export default React.memo(PlayingDocument)
