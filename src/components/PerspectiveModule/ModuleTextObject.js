c7df4a6160c153026091dc354f2310a772d70838
gffuma <fumagalli.gf@gmail.com>
2020-07-06 17:45:51 +0200

    Module text object handle layout object-text text-object

--- src/components/PerspectiveModule/ModuleTextObject.js
+++ src/components/PerspectiveModule/ModuleTextObject.js
@@ -3,13 +3,25 @@ import Markdown from 'markdown-to-jsx'
 import DocumentObject from './DocumentObject'
 
 export default function ModuleTextObject({ module }) {
-  return (
-    <div>
-      <DocumentObject
-        document={module.object.document}
-        caption={module.object.caption}
-      />
-      <Markdown>{module.text.content}</Markdown>
-    </div>
-  )
+  if (module.layout === 'text-object') {
+    return (
+      <div>
+        <Markdown>{module.text.content}</Markdown>
+        <DocumentObject
+          document={module.object.document}
+          caption={module.object.caption}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <DocumentObject
+          document={module.object.document}
+          caption={module.object.caption}
+        />
+        <Markdown>{module.text.content}</Markdown>
+      </div>
+    )
+  }
 }
