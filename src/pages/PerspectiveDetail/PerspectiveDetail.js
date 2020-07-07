e5285334f828c52ed20aa7d9e0c1a2ca80c196ca
gffuma <fumagalli.gf@gmail.com>
2020-07-07 10:33:25 +0200

    Chapters arrow

--- src/pages/PerspectiveDetail/PerspectiveDetail.js
+++ src/pages/PerspectiveDetail/PerspectiveDetail.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Menu from '../../components/Menu'
 import { useParams } from 'react-router-dom'
 import { useCacheStory } from '../../miller'
 import styles from './PerspectiveDetail.module.scss'
 import PerspectiveChapter from '../../components/PerspectiveChapter'
+import { ArrowLeft, ArrowRight } from 'react-feather'
 
 export default function PerspectiveDetail() {
   const { slug } = useParams()
   const [theme] = useCacheStory(slug, {
     withChapters: true,
   })
+  const chaptersRef = useRef()
 
   const { chapters } = theme.data
 
-  console.log(theme, chapters)
+  function getChapterWidth() {
+    const container = chaptersRef.current
+    const width = container.clientWidth
+    const chapterWidth = width * 0.4 + 2 // CSS width: 40% + 1px border x side
+    return chapterWidth
+  }
+  function handleScrollBackChapter() {
+    const container = chaptersRef.current
+    const chapterWidth = getChapterWidth()
+    const currentChapterIndex = Math.ceil(container.scrollLeft / chapterWidth)
+    container.scrollTo(Math.max(currentChapterIndex - 1, 0) * chapterWidth, 0)
+  }
+  function handleScrollNextChapter() {
+    const container = chaptersRef.current
+    const chapterWidth = getChapterWidth()
+    const currentChapterIndex = Math.floor(container.scrollLeft / chapterWidth)
+    container.scrollTo(
+      Math.min(currentChapterIndex + 1, chapters.length - 1) * chapterWidth,
+      0
+    )
+  }
 
   return (
     <div>
@@ -21,7 +43,23 @@ export default function PerspectiveDetail() {
       <h1>{theme.data.title}</h1>
       <p>{theme.data.abstract}</p>
 
-      <div className={styles.Chapters}>
+      <div className="d-flex justify-content-end">
+        <div>
+          <button
+            className="btn bg-transparent text-white"
+            onClick={handleScrollBackChapter}
+          >
+            <ArrowLeft />
+          </button>
+          <button
+            className="btn bg-transparent text-white"
+            onClick={handleScrollNextChapter}
+          >
+            <ArrowRight />
+          </button>
+        </div>
+      </div>
+      <div className={styles.Chapters} ref={chaptersRef}>
         {chapters.map((chapter) => (
           <div key={chapter.id} className={styles.Chapter}>
             <PerspectiveChapter chapter={chapter} />
