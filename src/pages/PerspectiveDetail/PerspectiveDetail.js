27dc60ee72ddb45e37aef9ea0978a38c72294263
uf0 <scnepa222@gmail.com>
2020-07-14 13:07:18 +0200

    style perstecive page

--- src/pages/PerspectiveDetail/PerspectiveDetail.js
+++ src/pages/PerspectiveDetail/PerspectiveDetail.js
@@ -1,13 +1,16 @@
 import React, { useRef } from 'react'
-import Menu from '../../components/Menu'
 import { useParams } from 'react-router-dom'
-import { useCacheStory } from '../../miller'
-import styles from './PerspectiveDetail.module.scss'
-import PerspectiveChapter from '../../components/PerspectiveChapter'
+import { useTranslation } from 'react-i18next'
 import { ArrowLeft, ArrowRight } from 'react-feather'
+import { useCacheStory } from '../../miller'
+import PerspectiveChapter from '../../components/PerspectiveChapter'
+import Menu from '../../components/Menu'
+import LangLink from '../../components/LangLink'
+import styles from './PerspectiveDetail.module.scss'
 
 export default function PerspectiveDetail() {
   const { slug } = useParams()
+  const { t } = useTranslation()
   const [theme] = useCacheStory(slug)
   const chaptersRef = useRef()
 
@@ -36,34 +39,63 @@ export default function PerspectiveDetail() {
   }
 
   return (
-    <div>
+    <React.Fragment>
       <Menu />
-      <h1>{theme.data.title}</h1>
-      <p>{theme.data.abstract}</p>
-
-      <div className="d-flex justify-content-end">
-        <div>
-          <button
-            className="btn bg-transparent text-white"
-            onClick={handleScrollBackChapter}
-          >
-            <ArrowLeft />
-          </button>
-          <button
-            className="btn bg-transparent text-white"
-            onClick={handleScrollNextChapter}
-          >
-            <ArrowRight />
-          </button>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-9">
+            <h1 className={`${styles.title} my-3`}>
+              <LangLink to="/perspectives" className="mr-3">
+                <ArrowLeft color="white"></ArrowLeft>
+              </LangLink>
+              {theme.data.title}
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="offset-md-4 col-md-7">
+            <p className={styles.description}>{theme.data.abstract}</p>
+          </div>
         </div>
       </div>
-      <div className={styles.Chapters} ref={chaptersRef}>
-        {chaptersIds.map((chapterId) => (
-          <div key={chapterId} className={styles.Chapter}>
-            <PerspectiveChapter chapterId={chapterId} />
+      <div className="h-100 d-flex flex-column overflow-hidden">
+        <div className="container flex-shrink-0">
+          <div className="row">
+            <div className="col-12 d-flex justify-content-between align-items-center">
+              <p className={`m-0 text-primary text-capitalize line-before`}>
+                {t('chapters')}
+              </p>
+              <div>
+                <button
+                  className="btn bg-transparent text-white"
+                  onClick={handleScrollBackChapter}
+                >
+                  <ArrowLeft />
+                </button>
+                <button
+                  className="btn bg-transparent text-white"
+                  onClick={handleScrollNextChapter}
+                >
+                  <ArrowRight />
+                </button>
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
+        <div
+          className={`${styles.chapters} d-flex flex-grow-1`}
+          ref={chaptersRef}
+        >
+          {chaptersIds.map((chapterId) => (
+            <div
+              key={chapterId}
+              className={`${styles.chapter} bg-white h-100 border-right`}
+            >
+              <PerspectiveChapter chapterId={chapterId} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
