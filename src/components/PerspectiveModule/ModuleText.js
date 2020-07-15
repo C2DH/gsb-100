8bf3fa1c6317bcea932ef33dd8a3ca24fe1da031
uf0 <scnepa222@gmail.com>
2020-07-15 14:59:19 +0200

    switch to react-markdown

--- src/components/PerspectiveModule/ModuleText.js
+++ src/components/PerspectiveModule/ModuleText.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Markdown from 'markdown-to-jsx'
+import ReactMarkdown from 'react-markdown'
 import classNames from 'classnames'
 
 export default function ModuleText({ module, className }) {
   return (
-    <Markdown className={classNames('my-2', className)}>
-      {module.text.content}
-    </Markdown>
+    <ReactMarkdown
+      source={module.text.content}
+      className={classNames('my-2', className)}
+      skipHtml={true}
+    ></ReactMarkdown>
   )
 }
