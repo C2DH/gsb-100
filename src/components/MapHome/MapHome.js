a50f3c15090b0c5e0b5b91ae182db09894767270
giorgio <giorgio@calib.ro>
2020-07-16 19:17:25 +0200

    Update MapHome.js

--- src/components/MapHome/MapHome.js
+++ src/components/MapHome/MapHome.js
@@ -2,8 +2,8 @@ import React, { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-const zoom = 5
-const center = [5.9714557, 50.4346632]
+const zoom = 4
+const center = [6.2, 50.4346632]
 
 export default function MapHome() {
   const mapContainer = useRef(null)
@@ -12,15 +12,15 @@ export default function MapHome() {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/satellite-v9', // stylesheet location
+      style: 'mapbox://styles/giorgiouboldi/ckcp21z5501qw1imwvmd6wyxo/draft', // stylesheet location
       center: center,
       zoom: zoom,
     })
 
     map.on('load', () => {
       map.flyTo({
-        zoom: 10,
-        speed: 0.2, // make the flying slow
+        zoom: 15,
+        speed: 0.3, // make the flying slow
         curve: 1, // change the speed at which it zooms out
       })
     })
