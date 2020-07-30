1fbde65f85eaad11f855c16d73936b8a989479b8
uf0 <scnepa222@gmail.com>
2020-07-30 14:19:46 +0200

    wip grid over

--- src/components/DocumentsGrid/DocumentGridItem.js
+++ src/components/DocumentsGrid/DocumentGridItem.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DocLink from '../DocLink'
 import IconSwitch from '../IconSwitch'
 import styles from './DocumentsGrid.module.scss'
 
 function DocumentGridItem({ doc }) {
+  const [show, setShow] = useState(false)
   const imageUrl = doc.data.resolutions?.thumbnail.url
 
   return (
-    <div className={styles.itemBlock}>
+    <div
+      className={styles.itemBlock}
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
       <DocLink document={doc}>
         {imageUrl ? (
-          <img title={doc.title} alt={doc.title} src={imageUrl} />
+          <img
+            className={styles.smallImage}
+            alt={doc.data.title}
+            src={imageUrl}
+          />
         ) : (
           <div className="w-100 h-100 border bg-secondary d-flex align-items-center justify-content-center">
             <IconSwitch color={'white'} type={doc.type}></IconSwitch>
           </div>
         )}
       </DocLink>
+      {show && (
+        <div className={styles.popover}>
+          <div>
+            <p className={`${styles.label} m-0 text-capitalize text-primary`}>
+              {doc.data.type}
+            </p>
+          </div>
+          <div className="flex-grow-1 flex-shrink-1 d-flex overflow-hidden flex-column my-2">
+            {imageUrl ? (
+              <img
+                className={styles.popoverImage}
+                alt={doc.data.title}
+                src={imageUrl}
+              />
+            ) : (
+              <div className="w-100 h-100 border bg-secondary d-flex align-items-center justify-content-center">
+                <IconSwitch color={'white'} type={doc.type}></IconSwitch>
+              </div>
+            )}
+          </div>
+          <div>
+            <p className="m-0">{doc.data.title}</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
