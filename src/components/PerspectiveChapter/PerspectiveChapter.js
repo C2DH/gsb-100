d546b492a623c2d7f0e1c049d9e3122450e068e2
uf0 <scnepa222@gmail.com>
2020-08-27 15:08:52 +0200

    minor on mofdules mobile

--- src/components/PerspectiveChapter/PerspectiveChapter.js
+++ src/components/PerspectiveChapter/PerspectiveChapter.js
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react'
-import { UncontrolledCollapse, CardBody, Card } from 'reactstrap'
+import React, { Suspense, useState } from 'react'
+import { Collapse, CardBody, Card } from 'reactstrap'
 import Media from 'react-media'
 import { ChevronDown, ChevronUp } from 'react-feather'
 import PerspectiveModule from '../PerspectiveModule'
@@ -17,18 +17,26 @@ const BREAKPOINTS = {
 function PerspectiveChapterContent({ chapterId }) {
   const [chapter] = useCacheStory(chapterId)
   const { modules } = chapter.contents
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
     <Media queries={BREAKPOINTS}>
       {(matches) =>
         matches.sm ? (
           <div>
-            <div className="d-flex align-items-center" id={`c_${chapterId}`}>
+            <div className="d-flex align-items-center" onClick={toggle}>
               <h3 className="m-0">{chapter.data.title}</h3>
               <div className="ml-auto">
-                <ChevronDown className="text-primary"></ChevronDown>
+                <ChevronDown
+                  className={`${styles.chevron} text-primary`}
+                  style={{
+                    transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+                  }}
+                ></ChevronDown>
               </div>
             </div>
-            <UncontrolledCollapse toggler={`#c_${chapterId}`}>
+            <Collapse isOpen={isOpen}>
               <Card className="border-0">
                 <CardBody className="p-0">
                   {modules.map((module, index) => (
@@ -38,7 +46,7 @@ function PerspectiveChapterContent({ chapterId }) {
                   ))}
                 </CardBody>
               </Card>
-            </UncontrolledCollapse>
+            </Collapse>
           </div>
         ) : (
           <React.Fragment>
@@ -55,13 +63,13 @@ function PerspectiveChapterContent({ chapterId }) {
   )
 }
 
-function PerspectiveChapter({ chapterId }) {
+function PerspectiveChapter({ chapterId, isOpen }) {
   return (
     <div
       className={`${styles.chapter} text-secondary px-md-4 pb-md-4 pt-md-0 pb-2 px-3 pt-2  position-relative`}
     >
       <Suspense
-        fallback={<h3 className="text-secondary">Loading ma chapter....</h3>}
+        fallback={<h3 className="text-secondary">Loading chapter....</h3>}
       >
         <PerspectiveChapterContent chapterId={chapterId} />
       </Suspense>
--- src/components/PerspectiveModule/ModuleUtils.js
+++ src/components/PerspectiveModule/ModuleUtils.js
@@ -4,7 +4,11 @@ import { CornerLeftUp } from 'react-feather'
 export const Caption = ({ caption }) => {
   return (
     <div className="d-flex align-items-start mt-2">
-      <CornerLeftUp size="14px" color={'#00b37f'}></CornerLeftUp>
+      <CornerLeftUp
+        size="14px"
+        color={'#00b37f'}
+        className="flex-shrink-0"
+      ></CornerLeftUp>
       <p className="ml-2 text-primary">{caption}</p>
     </div>
   )
