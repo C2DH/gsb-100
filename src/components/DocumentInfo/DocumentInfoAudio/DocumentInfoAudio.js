f087add6446d0304c70ea8cc9eacc92dd765ff6b
uf0 <scnepa222@gmail.com>
2020-09-03 17:44:57 +0200

    close #26

--- src/components/DocumentInfo/DocumentInfoAudio/DocumentInfoAudio.js
+++ src/components/DocumentInfo/DocumentInfoAudio/DocumentInfoAudio.js
@@ -6,8 +6,12 @@ export default function DocumentInfoAudio({ doc }) {
   const audioUrl = doc.url ? doc.url : doc.attachment
   return (
     <div className="h-100 d-flex flex-column">
-      <div className="flex-grow-1 d-flex align-items-center p-5">
-        {audioUrl && <AudioTrack url={audioUrl} />}
+      <div className="container py-5 h-50">
+        <div className="row h-100 align-items-center">
+          <div className="col-12 col-lg-10 offset-lg-1">
+            {audioUrl && <AudioTrack url={audioUrl} />}
+          </div>
+        </div>
       </div>
       <DocumentInfoBox doc={doc} />
     </div>
