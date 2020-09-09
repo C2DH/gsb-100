d31ca75670e5ee84c73d1169ce576cd3b469e2f5
uf0 <scnepa222@gmail.com>
2020-09-09 16:36:07 +0200

    #40

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import MenuResponsive from '../MenuResponsive'
 import styles from './NotFound.module.scss'
 
 export default function NotFound() {
+  const { t } = useTranslation()
   return (
     <div className="d-flex flex-column h-100">
       <MenuResponsive></MenuResponsive>
       <div className={styles.notFound}>
-        <h1 className="text-danger">Page not found</h1>
+        <h1 className="text-danger">{t('page not found')}</h1>
       </div>
     </div>
   )
--- src/components/PageError/PageError.js
+++ src/components/PageError/PageError.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import { useTranslation } from 'react-i18next'
 import NotFound from '../NotFound'
 import MenuResponsive from '../MenuResponsive'
 import styles from './PageError.module.scss'
 
 function Error({ message }) {
+  const { t } = useTranslation()
   return (
     <div className="d-flex flex-column h-100">
       <MenuResponsive></MenuResponsive>
       <div className={styles.notFound}>
-        <h1 className="text-danger">{message}</h1>
+        <h1 className="text-danger">{t(message)}</h1>
       </div>
     </div>
   )
@@ -32,10 +34,10 @@ export default class PageError extends Component {
         if (error.status === 404) {
           return <NotFound />
         }
-        return <Error message={`API error: ${error.status}`}></Error>
+        return <Error message={`API error`} status={error.status}></Error>
       }
       // You can render any custom fallback UI
-      return <Error message={`Something went wrong`}></Error>
+      return <Error message={`something went wrong`}></Error>
     }
 
     return this.props.children
