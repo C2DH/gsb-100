23e6d63816d05669ff952caa051077c6a8dae28d
uf0 <scnepa222@gmail.com>
2020-09-18 11:08:13 +0200

    added sub in perspectives

--- src/components/PerspectiveModule/DocumentObject.js
+++ src/components/PerspectiveModule/DocumentObject.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Video, AudioTrack } from './UniqueMedia'
 import YtVideo from '../YtVideo'
 import { Caption } from './ModuleUtils'
 import DocLink from '../DocLink'
 
 export default function DocumentObject({ document, caption, className }) {
+  const { i18n } = useTranslation()
+  const sub = useMemo(() => {
+    if (document.data.subtitles) {
+      const elm = document.data.subtitles.filter(
+        (d) => d.type === 'vtt' && d.language === i18n.language
+      )[0]
+      if (elm) {
+        return { url: elm.url, lang: i18n.language.split('_')[0] }
+      } else {
+        return { url: false }
+      }
+    } else {
+      return { url: false }
+    }
+  }, [i18n.language, document])
+
+  console.log(sub)
+
   if (document.type === 'image' || document.type === 'pdf') {
     const imagePreviewUrl = document.data.resolutions?.preview?.url
     return (
@@ -26,7 +45,7 @@ export default function DocumentObject({ document, caption, className }) {
   } else if (document.type === 'video') {
     const videoUrl = document.url ? document.url : document.data.translated_urls
     const yt = videoUrl.match(
-      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/
+      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w|-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/
     )
 
     return (
@@ -34,7 +53,7 @@ export default function DocumentObject({ document, caption, className }) {
         {videoUrl && yt ? (
           <YtVideo url={videoUrl}></YtVideo>
         ) : (
-          videoUrl && <Video url={videoUrl} />
+          videoUrl && <Video url={videoUrl} sub={sub} />
         )}
         <DocLink document={document}>
           <Caption caption={caption}></Caption>
