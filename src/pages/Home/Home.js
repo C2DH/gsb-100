8cd7d5b5f8b2ed795ae824b1564221100dfb74c5
uf0 <scnepa222@gmail.com>
2020-10-01 12:53:53 +0200

    subtitles and style on home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import ReactPlayer from 'react-player'
 import classNames from 'classnames'
-import { Play, ArrowRight } from 'react-feather'
+import { Volume2, ArrowRight, RotateCw } from 'react-feather'
 import { Transition } from 'react-spring/renderprops'
 import { animated } from 'react-spring'
 import { useCacheStory, useCacheDocument } from '../../miller'
@@ -14,11 +14,13 @@ import styles from './Home.module.scss'
 import LangLink from '../../components/LangLink'
 
 export default function Home() {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const [homeStory] = useCacheStory('home')
   const [document] = useCacheDocument(350) //HC forever
   const playerRef = useRef()
   const [showVideo, setShowVideo] = useState(false)
+  const [iconReplay, setIconReplay] = useState(false)
+  const [cue, setCue] = useState([])
   const [showPlay, setShowPlay] = useState(false)
   const [playing, setPlaying] = useState(false)
   const [muted, setMuted] = useState(true)
@@ -30,9 +32,48 @@ export default function Home() {
     playerRef.current.seekTo(0, 'fraction')
   }
 
+  const setSubtitles = (e) => {
+    const selected = e.target.activeCues[0]?.track.mode
+    const subtitles = Array.from(e.target.activeCues).map((cue) => cue.text)
+    if (selected !== 'hidden') {
+      setCue(subtitles)
+    }
+  }
+
   const onEnded = () => {
     setShowPlay(true)
+    setIconReplay(true)
   }
+
+  const onReady = () => {
+    selectSubtitle(i18n.language.split('_')[0])
+    const video = playerRef.current.wrapper.querySelector('video')
+    for (var i = 0; i < video.textTracks.length; i++) {
+      video.textTracks[i].addEventListener('cuechange', setSubtitles)
+    }
+  }
+
+  const selectSubtitle = (lang) => {
+    const video = playerRef.current.wrapper.querySelector('video')
+    for (var i = 0; i < video.textTracks.length; i++) {
+      video.textTracks[i].mode =
+        lang === video.textTracks[i].language ? 'showing' : 'hidden'
+      video.textTracks[i].default =
+        lang === video.textTracks[i].language ? true : false
+    }
+  }
+
+  const tracks = useMemo(() => {
+    return document.data.subtitles
+      ?.filter((d) => d.type === 'vtt')
+      .map((d) => {
+        return {
+          kind: 'metadata',
+          src: d.url,
+          srcLang: d.language.split('_')[0],
+        }
+      })
+  }, [document])
 
   useEffect(() => {
     if (zoomed) {
@@ -41,6 +82,21 @@ export default function Home() {
       setShowPlay(true)
     }
   }, [zoomed])
+
+  useMemo(() => {
+    playerRef.current && selectSubtitle(i18n.language.split('_')[0])
+  }, [i18n.language, playerRef])
+
+  useEffect(() => {
+    const video = playerRef.current.wrapper.querySelector('video')
+    return () => {
+      if (video) {
+        for (var i = 0; i < video.textTracks.length; i++) {
+          video.textTracks[i].removeEventListener('cuechange', setSubtitles)
+        }
+      }
+    }
+  })
 
   return (
     <React.Fragment>
@@ -56,6 +112,12 @@ export default function Home() {
         playsinline
         muted={muted}
         onEnded={onEnded}
+        onReady={onReady}
+        config={{
+          file: {
+            tracks: tracks,
+          },
+        }}
       />
       <div
         className={classNames(styles.mapContainer, {
@@ -103,12 +165,21 @@ export default function Home() {
                 }}
                 style={props}
               >
-                <Play size={30} />
+                {iconReplay ? (
+                  <RotateCw size={30}></RotateCw>
+                ) : (
+                  <Volume2 size={30} />
+                )}
               </animated.div>
             ))
           }
         </Transition>
-        <div className="d-flex justify-content-between justify-content-lg-center position-relative p-4 p-lg-0 mt-auto mt-lg-0">
+        <div className={`${styles.subtitles} mt-auto mb-2 px-4`}>
+          {cue.map((sub, index) => (
+            <p key={index}>{sub}</p>
+          ))}
+        </div>
+        <div className="d-flex justify-content-between justify-content-lg-center position-relative px-4 pb-4 pb-lg-0">
           <LangLink
             className={classNames(
               'btn btn-primary d-flex align-items-center text-uppercase',
